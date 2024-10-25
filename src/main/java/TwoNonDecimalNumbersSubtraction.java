b703f298e2a95fb39213c1eb3d5048aaa11ada0a
monishprasadg <monishprasadg@gmail.com>
2024-10-25 13:14:25 +0530

    Added solution for assgn 3

---
+++ src/main/java/TwoNonDecimalNumbersSubtraction.java
@@ -0,0 +1,27 @@
+package org.anudip.javaapp03.operation;
+
+public class TwoNonDecimalNumbersSubtraction{
+
+        // this methord is to Subtract 2 integer values
+
+    public int subtract(int num1, int num2) {
+        // Check for negative numbers
+        int result = 0;
+
+        if ((num1 < 0) || (num2 < 0)) {
+            result = -1;
+        }
+
+        // Check for zero values
+        else if (num1 == 0 || num2 == 0) {
+            result = -2;
+        }
+
+        // Perform Subtract and return the result
+        else {
+        result = num1-num2;// here the veriables are converted in double type
+
+        }
+        return result;        
+     }
+}
