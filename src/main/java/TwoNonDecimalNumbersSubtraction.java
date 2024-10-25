8741f230f0593c2bc934c2b24ad23d4fd23adada
Jnanesh2 <jnaneshgowda.2325@gmail.com>
2024-10-25 19:28:20 +0530

    Added Assignment 3 solutions

---
+++ src/main/java/TwoNonDecimalNumbersSubtraction.java
@@ -0,0 +1,19 @@
+package org.anudip.javaapp03.operation;
+
+public class TwoNonDecimalNumbersSubtraction {
+public long subtract(int num1, int num2) {
+int result = 0;
+if((num1 < 0) || (num2<0)) {
+result = -1;
+}
+else if ((num1 == 0) || (num2==0)) 
+{
+result = -2;
+}
+else
+{
+result = num1 - num2;
+}
+return result;
+}
+}
--- src/main/java/org/anudip/App.java
+++ src/main/java/org/anudip/App.java
@@ -1,7 +1,35 @@
 package org.anudip;
 
-public class App {
-	public static void main(String[] args) {
-		
-	}
+import org.anudip.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
+
+public class App
+{
+public static void main(String[] args)
+{
+TwoNonDecimalNumbersSubtraction twonondecimalnumberssubtraction = new TwoNonDecimalNumbersSubtraction();
+
+long result1 = twonondecimalnumberssubtraction.subtract(10,2);
+System.out.println(result1);
+
+long result2 = twonondecimalnumberssubtraction.subtract(2,10);
+System.out.println(result2);
+
+long result3 = twonondecimalnumberssubtraction.subtract(10,-2);
+System.out.println(result3);
+
+long result4 = twonondecimalnumberssubtraction.subtract(-10,2);
+System.out.println(result4);
+
+long result5 = twonondecimalnumberssubtraction.subtract(-10,-2);
+System.out.println(result5);
+
+long result6 = twonondecimalnumberssubtraction.subtract(10,0);
+System.out.println(result6);
+
+long result7 = twonondecimalnumberssubtraction.subtract(0,2);
+System.out.println(result7);
+
+long result8 = twonondecimalnumberssubtraction.subtract(0,0);
+System.out.println(result8);
+}
 }
