2e792639f0761ceaaa04fe54fb4510fab599481b
AdithyaS <adithyawalvekar@gmail.com>
2024-10-25 17:26:21 +0530

    Added Solution

---
+++ src/main/java/TwoNonDecimalNumbersSubtraction.java
@@ -0,0 +1,28 @@
+package org.anudip.javaapp03.operation;
+
+
+public class TwoNonDecimalNumbersSubtraction {
+
+        // this methord is to multiply 2 integer values
+
+    public int subtract(int num1, int num2) {
+        // Check for negative numbers
+        int result = 0;
+
+        if ((num1 < 0) || (num2 < 0)) {
+            result = -1;
+        }
+
+        // Check for zero values
+        else if (num1 == 0 || num2 == 0) {
+            result = -2;
+        }
+
+        // Perform multiplication and return the result
+        else {
+        result = num1 - num2;
+
+        }
+        return result;
+     }
+}
